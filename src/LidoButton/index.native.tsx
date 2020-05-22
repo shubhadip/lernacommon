d89447a1b542c0e827b0e647046170d4095f8ade
shubhadip <shubhadip.maity@gmail.com>
2020-05-22 16:42:11 +0530

    linting prettier added

--- src/LidoButton/index.native.tsx
+++ src/LidoButton/index.native.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
-import { View, TouchableOpacity, ActivityIndicator } from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  Button,
+  ActivityIndicator
+} from 'react-native';
 
 import AppText from '../LidoAppText/index.native';
 
